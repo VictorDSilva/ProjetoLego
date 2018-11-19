f8e3656fbf35da64260687dbe001125a29c48bd0
Kevin Azevedo <kevinazevedo257@gmail.com>
2018-11-18 22:18:39 -0200

    pivot table

--- app/Kit.php
+++ app/Kit.php
@@ -4,7 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Kit extends Model
 {
-    // TODO relação
+    public function pecas() {
+        return $this->belongsToMany('App\Peca', 'kit_peca');
+    }
 }
